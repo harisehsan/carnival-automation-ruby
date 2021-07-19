83434887f8fc7724fee37e2cd7bbd7cd10d1b693
harisehsan <38343702+harisehsan@users.noreply.github.com>
2021-07-19 20:47:13 +0500

    Update google_search_spec.rb

--- spec/features/specs/google_search_spec.rb
+++ spec/features/specs/google_search_spec.rb
@@ -3,16 +3,21 @@
 require 'pry'
 require_relative '../../spec_helper'
 
-feature 'Google - Search', type: :feature, js: true do
+feature 'User is able to filter and narrow results from landing page and then start the booking process.', type: :feature, js: true do
 
-  scenario 'visiting the Google home page and searching for the word "capybara"' do
+  scenario 'TC1 Verify that SAIL TO and DURATION filters are narrowing the results' do
     home_page.load
-    home_page.search_for(text: 'capybara')
+    home_page.skip_popup
+    home_page.narrow_results_by_filter
+    expect(home_page.compare_number_of_results)
+  end
 
-    expect(home_page).to have_content 'Capybara - Wikipedia'
-
-    Capybara::Screenshot.screenshot_and_save_page
-    Capybara::Screenshot.screenshot_and_open_image
+  scenario 'TC2 Verify that {{secondary_filter}} filter is narrowing the results after applying SAIL TO and DURATION filters' do
+    home_page.load
+    home_page.skip_popup
+    home_page.narrow_results_by_filter
+    home_page.go_to_url
+    expect(home_page.verify_the_price_filter)
   end
 
 end
