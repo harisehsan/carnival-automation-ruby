0ab4461bcd4bca9ae4f34f150c578c6876721744
harisehsan <38343702+harisehsan@users.noreply.github.com>
2021-07-20 23:26:05 +0500

    Update home.rb

--- spec/features/pages/home.rb
+++ spec/features/pages/home.rb
@@ -16,6 +16,16 @@ module Pages
     element :search_number_lbl, '.sbsc-container__result-count'
     elements :search_results_wigits, 'article'
     element :first_item_price, 'body > div:nth-child(11) > form:nth-child(2) > div:nth-child(8) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ccl-cruise-search:nth-child(1) > div:nth-child(4) > ccl-view-result-container:nth-child(3) > div:nth-child(1) > ccl-view-result-list:nth-child(1) > article:nth-child(1) > ccl-view-result-list-item:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > ul:nth-child(1) > li:nth-child(1) > p:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2) > span:nth-child(1)'
+    element :view_item, 'body > div:nth-child(11) > form:nth-child(2) > div:nth-child(8) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ccl-cruise-search:nth-child(1) > div:nth-child(4) > ccl-view-result-container:nth-child(3) > div:nth-child(1) > ccl-view-result-list:nth-child(1) > article:nth-child(3) > ccl-view-result-list-item:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > a:nth-child(4)'
+    element :review_trip_button, '.diagonal-button-new > span'
+    element :save_label_btn,'.be-itinerary__save-image'
+    element :continue_btn,'.be-btn.be-btn--primary.be-btn--large.be-hdr-d__col-continue'
+    element :traveler_information_lbl,'#ges-heading'
+    elements :price_lbl,'span.vrl-item__price'
+    element :bahamas_btn,'.cdc-filter-5-cols-layout:nth-child(2) > .cdc-filter-button'
+    element :canada_btn, '.cdc-filter-5-cols-layout:nth-child(4) > .cdc-filter-button'
+    element :caribbean_btn, '.cdc-filter-5-cols-layout:nth-child(5) > .cdc-filter-button'
+    elements :load_more_btn, '.vrcn-load-more__text.ng-binding'
 
 
 
@@ -54,7 +64,32 @@ module Pages
     def verify_the_price_filter
       first_item_price.text.delete("^0-9").to_i < MaxPrice
     end
+
+    def view_details_of_iternity
+      view_item.click
+      review_trip_button.click
+      save_label_btn.click
+      continue_btn.click
+    end
+    def verify_the_traveler_information_title
+      traveler_information_lbl.text =='TRAVELER INFORMATION'
+    end
+
+  def apply_filter_for_more_results
+    sail_to_dropdown.click
+    bahamas_btn.click
+    bermuda_button.click
+    canada_btn.click
+    caribbean_btn.click
+    duration_dropdown.click
+    six_to_nine_days_button.click
+    search_button.click
   end
+
+  def verify_load_more_button
+    load_more_btn.size > 0
+  end
+    end
   end
 
 module PageNameHelpers
